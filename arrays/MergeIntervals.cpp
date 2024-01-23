b8c07e7c141ece069cd49d8467550850f6b44ce7
kavyansh bansal <kavyansheduv@gmail.com>
2024-01-22 20:55:02 -0800

    mergeIntervals

---
+++ arrays/MergeIntervals.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+
+        sort(intervals.begin(),intervals.end());
+
+        int mergingIntervalStart = intervals[0][0];
+        int mergingIntervalEnd = intervals[0][1];
+
+
+        vector<vector<int>> ans;
+
+        for(int i = 1; i < intervals.size(); i++) {
+            vector<int> currentInterval = intervals[i];
+            int newIntervalStart = currentInterval[0];
+            int newIntervalEnd = currentInterval[1];
+
+            if (mergingIntervalEnd >= newIntervalStart) {
+                mergingIntervalEnd = max(newIntervalEnd,mergingIntervalEnd;
+            }else{
+                ans.push_back({mergingIntervalStart, mergingIntervalEnd});
+                mergingIntervalStart = newIntervalStart;
+                mergingIntervalEnd = newIntervalEnd;
+            }
+
+        }
+
+        ans.push_back({mergingIntervalStart, mergingIntervalEnd});
+
+        return ans;
+
+        
+        
+    }
+};
